19e5a8ff8d706c0323ff29ed8428f435ebc2bc80
Ishika-D <ishikadawda2000@gmail.com>
2023-05-14 20:23:56 +0530

    Added various application components

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import { ArrowDown, Eye, EyeOff } from "react-feather";
+
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
+
+import styles from "./Body.module.css";
+
+function Body() {
+  return (<div></div>);
+}
---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { X } from "react-feather";
+
+import InputControl from "../InputControl/InputControl";
+
+import styles from "./Editor.module.css";
+
+function Editor(props) {
+  return (<div></div>);
+}
+
+export default Editor;
